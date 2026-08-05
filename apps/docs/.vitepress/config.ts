f5452c81eef9651bf0ed3a89dd24c857341d23aa
Ruth Hilary <akaadouruth@gmail.com>
2026-08-05 18:44:29 +0100

    feat: add Kharis dove logo as favicon for docs and web app

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   ignoreDeadLinks: [/^http:\/\/localhost/],
 
   head: [
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon.png' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     ['link', {
